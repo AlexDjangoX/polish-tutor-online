6a3f077e8f5112cfe98be8df1df5386e56541faf
AlexDjangoX <alexmonk17@gmail.com>
2023-05-29 18:51:23 +0200

    Updated placeholder text in Contact.jsx

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -147,7 +147,7 @@ const Contact = () => {
               value={formData.message}
               onChange={handleInputChange}
               className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 h-40 resize-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-              placeholder="Your message here..."
+              placeholder="Book a free 50 minutes lesson here. I will contact you ASAP!"
               required
             />
           </div>
